91cd9cbfb299284ba7399a327e0b4d13b611e953
Alexey Osipov <aoosipov88@gmail.com>
2021-05-13 02:11:55 +0000

    new files

--- hello.js
+++ hello.js
@@ -3,3 +3,12 @@ const sayHello  = function () {
 }
 
 sayHello();
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
